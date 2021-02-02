d29cb569e334aa3e298b55ea6abcf20447bf2a1c
Johannes <johannes.thessen@gmail.com>
2021-02-02 18:35:59 +0100

    Messed around with elint. Had to remove again. Hopefully works

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const url = "https://memories-portfolio-project.herokuapp.com/posts";
+const url = "http://localhost:5000/posts";
+
+// Heroku
+// "https://memories-portfolio-project.herokuapp.com/posts"
+
+// Local
+// "http://localhost:5000/posts"
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
